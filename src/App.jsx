05fd9d1610d4d8fa45fbf1323cbaac3b6596a670
Isaac <ederaldohuss@gmail.com>
2025-09-02 15:42:19 -0300

    terceiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
 
   return (
     <>     
-      <div>
-       
+      <div className='envolver_tudo'>
+       <div className='foto_bola'>
+
+       </div>
       </div>     
     </>
   )
