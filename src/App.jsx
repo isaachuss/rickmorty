dbc4dd20151ae1dd7e2ba3403123c2d101633207
isaac <ederaldohuss@gmail.com>
2025-09-05 11:45:38 -0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,17 @@ function App() {
   return (
     <>     
       <div className='envolver_tudo'>
-       <div className='foto_bola'>
-
-       </div>
+       <div className='foto_bola'></div>
+      <div className='div_nome'>
+        <h2 className='texto1'>Nome: Evil Morty</h2>
+        <h2 className='texto1'>Status: Vivo</h2>
+        <h2 className='texto1'>Especie: Humano</h2>
+        <h2 className='texto1'>Tipo: Marionete sem alma</h2>
+        <h2 className='texto1'>Gênero: Masculino</h2>
+        <h2 className='texto1'>Localização: Ricks’s Story</h2>
+        <h2 className='texto1'>Ultimo ep que foi visto: Unmortricken</h2>
+      </div>
+       
       </div>     
     </>
   )
