2b47782b0a483fe551c48793bf24fbc5aedc8ed7
isaac <ederaldohuss@gmail.com>
2025-09-23 15:42:24 -0300

    commit 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
+
+useEffect ( () => {
+    fetch('https://rickandmortyapi.com/api/character/'+count)
+    .then(res => console.log(res.))
+    .catch(error => console.log('error'))
+  }, [count]);
 
   return (
     <>     
