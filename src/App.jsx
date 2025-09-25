aec2db7a1a01c9d75054cc90e415734993dd4aa9
isaac <ederaldohuss@gmail.com>
2025-09-25 14:49:30 -0300

    commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,45 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(1)
+  const [character, setCharacter] = useState(null);
 
-useEffect ( () => {
-    fetch('https://rickandmortyapi.com/api/character/'+count)
-    .then(res => console.log(res.))
-    .catch(error => console.log('error'))
-  }, [count]);
+useEffect(() => {
+  fetch(`https://rickandmortyapi.com/api/character/${count}`)
+    .then(res => res.json())
+    .then(data => setCharacter(data))
+    .catch(error => console.log('error', error));
+}, [count]);
 
-  return (
-    <>     
-      <div className='envolver_tudo'>
-       <div className='foto_bola'></div>
-      <div className='div_nome'>
-        <h2 className='texto1'>Nome: Evil Morty</h2>
-        <h2 className='texto1'>Status: Vivo</h2>
-        <h2 className='texto1'>Especie: Humano</h2>
-        <h2 className='texto1'>Tipo: Marionete sem alma</h2>
-        <h2 className='texto1'>Gênero: Masculino</h2>
-        <h2 className='texto1'>Localização: Ricks’s Story</h2>
-        <h2 className='texto1'>Ultimo ep que foi visto: Unmortricken</h2>
+
+return (
+  <>
+    <div className='envolver_tudo'>
+      {character && (
+        <>
+          <div className='foto_bola'>
+            <img className='img' src={character.image} alt={character.name} />
+          </div>
+
+          <div className='div_nome'>
+            <h4 className='texto1'>Nome: {character.name}</h4>
+            <h4 className='texto1'>Status: {character.status}</h4>
+            <h4 className='texto1'>Espécie: {character.species}</h4>
+            <h4 className='texto1'>Tipo: {character.type || "Não definido"}</h4>
+            <h4 className='texto1'>Gênero: {character.gender}</h4>
+            <h4 className='texto1'>Localização: {character.location?.name}</h4>
+            <h4 className='texto1'>Último ep: {character.episode?.slice(-1)[0]}</h4>
+          </div>
+        </>
+      )}
+
+      <div className="button-get">
+        <button className='button' onClick={() => setCount(count + 1)}>
+          Gerar um novo personagem
+        </button>
       </div>
-       
-      </div>     
-    </>
-  )
+    </div>
+  </>
+);
 }
 
 export default App
